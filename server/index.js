5bd90ff8b4c4d6cfc7fe9f8ed17fc4d48501cd27
VanshSrivastava200 <vanshsrivastava20022005@gmail.com>
2026-01-22 06:28:44 +0530

    final

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://vchessplay.netlify.app/",
+    origin: ["https://vchessplay.netlify.app/","http://localhost:3000"],
     credentials: true,
   })
 );
